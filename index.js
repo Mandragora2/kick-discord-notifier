4fc580265db46e96f3d79134cf552b19a27b41ee
Mandragora <djff@free.fr>
2023-05-12 10:02:04 +0200

    feat(stats): Add stats for calls

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 let isLive = false;
 let messageSent = false;
+let index = 0;
+let retries = 0;
+const stats = {
+	success: 0,
+	error: 0,
+	total: 0,
+};
 
 const livestreamUrl = 'https://kick.com/api/v1/channels/' + process.env.STREAMER_SLUG;
 
@@ -16,11 +23,12 @@ function poll(chan) {
 	const headers = {
 		'Accept': 'text/html,application/xhtml+xml,application/xml',
 		'Accept-Encoding': 'gzip, deflate',
-		'User-Agent': 'Firefox/108.0',
+		'User-Agent': 'Firefox/108.0' + index,
 		'Cookie': '',
 	};
 
 	console.log('Polling kick…');
+	index++;
 
 	axios.get(livestreamUrl, {
 		headers: headers,
@@ -28,6 +36,8 @@ function poll(chan) {
 		httpsAgent: new https.Agent({ keepAlive: false }),
 	})
 		.then((response) => {
+			stats.success++;
+			retries = 0;
 			const botUser = chan.client.user;
 			data = response.data;
 			if (data.livestream === null) {
@@ -44,19 +54,42 @@ function poll(chan) {
 
 			console.log('Streamer is live, send message.');
 
+			const date = new Date(data.livestream.created_at);
+
+			console.log(data.livestream);
+
 			const embed = new EmbedBuilder()
 				.setTitle(data.livestream.session_title)
 				.setURL(`https://kick.com/${data.slug}`)
 				.setDescription(`https://kick.com/${data.slug}`)
 				.setThumbnail(data.user.profile_pic)
 				.setImage(data.livestream.thumbnail.url)
-				.setTimestamp(new Date(data.livestream.created_at).getTime())
+				.setTimestamp(new Date(date.getTime() - (date.getTimezoneOffset() * 60 * 1000)))
 				.setFooter({ text: 'Kick' })
         ;
 
-			chan.send({ embeds: [embed], content: '<:Kick_29:1097568203963650089> LIVE ON SUR KICK <:Kick_29:1097568203963650089>' });
+			chan.send({ embeds: [embed], content: `@everyone <:Kick:1101247960320327702> LIVE ON SUR KICK <:Kick:1101247960320327702>\n\n➡️ <https://kick.com/${data.slug}> ⬅️\n\n` });
 			messageSent = true;
+			isLive = true;
 			botUser.setPresence({ activities: [{ name: `${data.user.username} sur Kick`, type: 3, url: `https://kick.com/${data.slug}` }] });
+		},
+		(err) => {
+			stats.error++;
+			retries++;
+			console.error(`Error: ${err.response.status} / ${err.response.statusText}. Retry: ${retries}`);
+		})
+		.finally(() => {
+			stats.total++;
+			const botUser = chan.client.user;
+			if (retries > 0) {
+				botUser.setPresence({ activities: [{ name: 'Kick API (unstable)', type: 0 }] });
+			}
+			else if (retries === 0 && !isLive) {
+				botUser.setPresence({ activities: [{ name: 'Kick API is ok \\o/', type: 0 }] });
+			}
+			if (stats.total % 60 === 0) {
+				console.log(`Call stats: ${stats.total} calls, ${(stats.success * 100 / stats.total).toFixed(2)}% success, ${(stats.error * 100 / stats.total).toFixed(2)}% error.`);
+			}
 		});
 }
 
